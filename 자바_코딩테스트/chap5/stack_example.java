3d11d153133670535a031fb600cd5b92c24fe646
dasoopark <ektn0514@gmail.com>
2021-04-16 05:25:13 +0900

    chap5_1_stack
    stack example in Java

---
+++ 자바_코딩테스트/chap5/stack_example.java
@@ -0,0 +1,28 @@
+package chap5;
+
+import java.util.Stack;
+
+public class stack_example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Stack<Integer> s = new Stack<>();
+
+	        // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+	        s.push(5);
+	        s.push(2);
+	        s.push(3);
+	        s.push(7);
+	        s.pop();
+	        s.push(1);
+	        s.push(4);
+	        s.pop();
+	        // 스택의 최상단 원소부터 출력
+	        while (!s.empty()) {
+	            System.out.println(s.peek());
+	            s.pop();
+	        }
+
+	}
+
+}

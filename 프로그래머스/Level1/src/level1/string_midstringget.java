8ae2f55b76fe5c2b0cd00a4a17975cc16fed2bf0
dasoopark <ektn0514@gmail.com>
2021-07-14 19:21:37 +0900

    가운데 문자열 출력
    문제 설명
    단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
    
    재한사항
    s는 길이가 1 이상, 100이하인 스트링입니다.
    입출력 예
    s	return
    "abcde"	"c"
    "qwer"	"we"

--- 프로그래머스/Level1/src/level1/string_midstringget.java
+++ 프로그래머스/Level1/src/level1/string_midstringget.java
@@ -1,9 +1,41 @@
 package level1;
 
+
+//가운데 글자 가져오기
+
+/*
+문제 설명
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+입출력 예
+s			return
+"abcde"		"c"
+"qwer"		"we"
+*/
 public class string_midstringget {
 
+	static String answer(String s)
+	{
+		 String answer = "";
+	        
+	        if(s.length()%2==0)
+	        {
+	            answer = s.substring(s.length()/2-1,s.length()/2+1);
+	        }
+	        else
+	        {
+	            answer = s.substring(s.length()/2,s.length()/2+1);
+	        }
+	        
+		return answer;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String test ="abcde";
+		String test2 = "qwer";
+		System.out.println(answer(test));
+		System.out.println(answer(test2));
 
 	}
 

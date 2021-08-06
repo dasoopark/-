87b614b240424775d3f316f27c0f6eabfb6b5283
dasoopark <ektn0514@gmail.com>
2021-08-06 23:54:15 +0900

    Create musicvideo_choicealgorithm_09.java

---
+++ 프로그래머스/Level1/src/algorithm_Study/Sorting_and_Searching_sec6/musicvideo_choicealgorithm_09.java
@@ -0,0 +1,10 @@
+package algorithm_Study.Sorting_and_Searching_sec6;
+
+public class musicvideo_choicealgorithm_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

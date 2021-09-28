d668707b5527bc963f132d5a3b2f219a826231b9
dasoopark <ektn0514@gmail.com>
2021-09-29 02:44:41 +0900

    Update mirror_search_10.java

--- 프로그래머스/Level1/src/algorithm_Study/DFS_BFS_sec8/mirror_search_10.java
+++ 프로그래머스/Level1/src/algorithm_Study/DFS_BFS_sec8/mirror_search_10.java
@@ -44,12 +44,43 @@ public class mirror_search_10 {
 	static int[] dy = {0, 1, 0, -1};
 	static int[][] board;
 	static int answer = 0;
+	
+	static void DFS(int x, int y)
+	{
+		if(x==7 && y==7)
+		{
+			answer++;
+		}
+		else
+		{
+			for(int i=0; i<4; i++)
+			{
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				if(nx>=1 && nx<=7 && ny>=1 && ny<=7 &&  board[nx][ny]==0) //경계선 안쪽
+				{
+					board[nx][ny] = 1; // 갓던길 체크 
+					DFS(nx, ny); 
+					board[nx][ny] = 0; //통로로 풀어줘야함1
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb = new Scanner(System.in);
 		board = new int[8][8];
-		
+		for(int i=1; i<=7;i++)
+		{
+			for(int j=1; j<=7;j++)
+			{
+				board[i][j] = kb.nextInt();
+			}
+		}
+		board[1][1] = 1;
+		DFS(1, 1);
+		System.out.println(answer);
 
 	}
 

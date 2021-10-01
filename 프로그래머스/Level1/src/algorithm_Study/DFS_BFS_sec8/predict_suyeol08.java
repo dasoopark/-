e7d4de9c1c90ec07ee53e76e4994f945d4090b7a
dasoopark <ektn0514@gmail.com>
2021-10-01 23:55:59 +0900

    Update predict_suyeol08.java

--- 프로그래머스/Level1/src/algorithm_Study/DFS_BFS_sec8/predict_suyeol08.java
+++ 프로그래머스/Level1/src/algorithm_Study/DFS_BFS_sec8/predict_suyeol08.java
@@ -1,5 +1,7 @@
 package algorithm_Study.DFS_BFS_sec8;
 
+import java.util.Scanner;
+
 /*
  8. 수열 추측하기
 설명
@@ -35,10 +37,30 @@ N은 가장 윗줄에 있는 숫자의 개수를 의미하며 F는 가장 밑에 줄에 있는 수로 1,000,0
 3 1 2 4 
  */
 public class predict_suyeol08 {
-
+	static int[] b, p, ch;
+	static int n, f;
+	boolean flag = false;
+	static int[][] dy = new int[35][35];
+	static int combi(int n, int r)
+	{
+		if(dy[n][r]>0)
+		{
+			return dy[n][r];
+				
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner kb = new Scanner(System.in);
+		n = kb.nextInt();
+		f = kb.nextInt();
+		b = new int[n];
+		p = new int[n];
+		ch = new int[n+1];
+		for(int i=0;i<n;i++)
+		{
+			b[i] = combi(n-1, i);
+		}
 	}
 
 }
